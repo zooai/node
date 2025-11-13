236f3ee6dd44a69c340347b535fbffee45b8ee1e
Zach Kelling <z@zeekay.io>
2025-11-13 10:56:16 -0800

    v1.1.33: Remove build.rs (zoo-node doesn't need asset copying)
    ERROR: build.rs:4 - use of unresolved module zoo_tools_runner
    FILE: zoo-bin/zoo-node/build.rs
    
    ROOT CAUSE: build.rs calls zoo_tools_runner::copy_assets::copy_assets
    We removed zoo_tools_runner from [build-dependencies] in v1.1.32
    But build.rs file still existed and tried to use it
    
    FIX: Deleted zoo-bin/zoo-node/build.rs entirely
    - Zoo-node doesn't need to bundle/copy tool runner resources
    - Assets are handled at runtime, not build time
    - Hanzo-node needs build.rs for bundling, zoo-node doesn't
    
    THIS IS THE FINAL FIX - no build script = no build-time dependencies!

--- zoo-bin/zoo-node/build.rs
+++
@@ -1,9 +0,0 @@
-use std::path::PathBuf;
-
-fn main() {
-    zoo_tools_runner::copy_assets::copy_assets(
-        Some(PathBuf::from("./")),
-        Some(PathBuf::from("../../target").join(std::env::var("PROFILE").unwrap())),
-    )
-    .expect("failed to copy zoo-tools-runner assets");
-}
